83f6567b69f61bc37f23867a928f0105fc86d605
Vincenzo Picone <vincenzopicone88@icloud.com>
2023-02-09 13:43:28 +0100

    Start

---
+++ epibooks/src/components/MyJumbotron.jsx
@@ -0,0 +1,10 @@
+
+const MyJumbotron = () => (
+  <div class="container-fluid">
+        <h1 class="display-5 fw-bold">Epibooks</h1>
+        <p class="col-md-8 fs-4">Epibooks with React</p>
+        <button class="btn btn-primary btn-lg" type="button">More Info</button>
+      </div>
+)
+
+export default MyJumbotron
